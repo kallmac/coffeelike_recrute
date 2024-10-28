0526baeac652d65528638050cd08ca343db7ffd1
kallmac <124498434+kallmac@users.noreply.github.com>
2024-10-28 21:07:57 +0300

    вторая версия системы опроса

--- test.py
+++ test.py
@@ -10,13 +10,15 @@ questions = [
     ("Какой ваш любимый фильм?", 0),
     ("Какой ваш любимый вид спорта?", ["Футбол", "Баскетбол", "Теннис"])
 ]
-
+ispoll = False
 current_question_index = 0
 user_responses = {}
 
 @bot.message_handler(commands=['poll'])
 def poll_command(message):
+    global ispoll
     global current_question_index
+    ispoll = True
     current_question_index = 0
     user_responses[message.from_user.id] = []
     ask_question(message.chat.id)
@@ -34,18 +36,21 @@ def ask_question(chat_id):
                 markup.add(button)
             bot.send_message(chat_id, question, reply_markup=markup)
     else:
+        global ispoll
+        ispoll = False
         bot.send_message(chat_id, "Опрос завершен. Спасибо за участие!")
         current_question_index = 0
 
 @bot.message_handler(func=lambda message: True)
 def handle_text_response(message):
-    global current_question_index
-    if message.from_user.id in user_responses and current_question_index < len(questions) and questions[current_question_index][1] == 0:
-        response = message.text
-        user_responses[message.from_user.id].append((questions[current_question_index][0], response))
-        bot.send_message(message.chat.id, f"Спасибо за ваш ответ: {response}")
-        current_question_index += 1
-        ask_question(message.chat.id)
+    if ispoll:
+        global current_question_index
+        if message.from_user.id in user_responses and current_question_index < len(questions) and questions[current_question_index][1] == 0:
+            response = message.text
+            user_responses[message.from_user.id].append((questions[current_question_index][0], response))
+            bot.send_message(message.chat.id, f"Спасибо за ваш ответ: {response}")
+            current_question_index += 1
+            ask_question(message.chat.id)
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_query(call):
@@ -61,5 +66,5 @@ def handle_query(call):
         current_question_index += 1
         ask_question(call.message.chat.id)
         bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
-        #bot.edit_message_text(call.message.chat.id, call.message.message_id, questions[question_index][0] + "\nПИДОРЫ")
+        bot.edit_message_text(chat_id=call.message.chat.id, parse_mode='Markdown', message_id=call.message.message_id, text=questions[question_index][0] + "\n*✅Ваш ответ:* " + answer)
 bot.polling()
