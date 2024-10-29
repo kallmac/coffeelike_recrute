e4bc2ef348a44bd5991077788479e19f7031b3dd
Gas <artem_zelenskiy_07@mail.ru>
2024-10-29 13:49:21 +0300

    исправил созд. бд и добавил sqlite бд-и для админов и dev-ов

--- bd_main.py
+++ bd_main.py
@@ -2,11 +2,16 @@ import telebot
 from telebot import types
 import sqlite3
 
-conn = sqlite3.connect('с_users.sql')
+conn = sqlite3.connect('admins.sql')
 cur = conn.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS admins (id int auto_increment primary key, tg_id varchar(255), tg_username varchar(255), notif int)')
-cur.execute('CREATE TABLE IF NOT EXISTS devs (id int auto_increment primary key, tg_id varchar(255), tg_username varchar(255))')
 conn.commit()
 cur.close()
 conn.close()
 
+conn = sqlite3.connect('devs.sql')
+cur = conn.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS devs (id int auto_increment primary key, tg_id varchar(255), tg_username varchar(255))')
+conn.commit()
+cur.close()
+conn.close()
