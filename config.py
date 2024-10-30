ebd16fa527deb1e1f44c172159bf83d2cad99479
Gas <artem_zelenskiy_07@mail.ru>
2024-10-30 14:05:39 +0300

    Config: add_user; get user

--- config.py
+++ config.py
@@ -2,14 +2,26 @@ import sqlite3
 
 conn = sqlite3.connect('everyone.sql')
 cur = conn.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS devs (id int auto_increment primary key, tg_id varchar(255), tg_username varchar(255), status varchar(255), notif int)')
+cur.execute('CREATE TABLE IF NOT EXISTS everyone (id int auto_increment primary key, tg_id varchar(255), tg_username varchar(255), status varchar(255), notif int)')
 conn.commit()
 cur.close()
 conn.close()
 
 class UsersTable:
     def get_user(usr_id: int, self) -> dict:
-        pass
+        conn = sqlite3.connect('everyone.sql')
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM everyone WHERE tg_id == usr_id')
+        user_info = cur.fetchone() # массив с данными об 1 пользователе
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        user_dict = {'id': user_info[1],
+             'username': user_info[2],
+             'status': user_info[3],
+             'notif': user_info[4]}
+        return user_dict # возвращает словарь с инфой о пользователе
 
     def is_admin(usr_id, self) -> bool:
         pass
@@ -24,6 +36,12 @@ class UsersTable:
         pass
 
     def add_user(usr_data: dict, self):
+        conn = sqlite3.connect('everyone.sql')
+        cur = conn.cursor()
+        cur.execute('INSERT INTO everyone (tg_id, tg_username, status, notif)VALUES(%s, %s, %s, %s)' % (dict['id'], dict['username'], dict['status'], dict['notif']))
+        conn.commit()
+        cur.close()
+        conn.close()
         pass
 
     def del_usr(usr_id, self):
